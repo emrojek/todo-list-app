1b9776e15199b837db7897c4e418c430ee58713d
emrojek <michal.rojek.it@gmail.com>
2023-10-03 23:16:37 +0200

    add new version of the app

--- app.js
+++ app.js
@@ -1,22 +1,70 @@
 'use strict';
 
-const btnAdd = document.querySelector('.btn-task');
-const inputField = document.getElementById('task-row');
-const taskList = document.getElementById('task-container');
+// Selectors
+const formSubmit = document.getElementById('todo-form');
+const formInput = document.getElementById('task-row');
+const todoList = document.querySelector('.task-container');
+
+let todos = [];
+
+// Listeners
+formSubmit.addEventListener('submit', function (e) {
+	e.preventDefault();
+	newTodo();
+});
+
+todoList.addEventListener('click', function (e) {
+	if (e.target.classList.contains('text')) {
+		const todoKey = e.target.parentElement.dataset.key;
+		checkTodo(todoKey);
+	}
+});
 
 // Functions
-const addElement = function () {
-	if (inputField.value === '') {
-		inputField.placeholder = 'Write some text first!';
-	} else {
-		const newLi = document.createElement('li');
-		document.getElementById('task-container').appendChild(newLi);
-		newLi.innerHTML = `
-			<span>${inputField.value}</span>
-		`;
+const newTodo = () => {
+	const todo = {
+		text: formInput.value,
+		checked: false,
+		id: Date.now(),
+	};
+
+	todos.push(todo);
+
+	if (formInput.value === '') {
+		formInput.placeholder = 'Field cannot be empty!';
+		return;
 	}
-	inputField.value = '';
+
+	const html = `
+		<div class="list" data-key="${todo.id}" id="${todo.id}">
+			<span class="circle"><i class="circle-color fa-regular fa-circle"></i></span>
+			<span class="trash"><i class="trash-color fa-solid fa-trash-can"></i></span>
+			<span class="edit"><i class="edit-color fa-regular fa-pen-to-square"></i></span>
+			<span class="circle-check"><i class="check-color fa-solid fa-circle-check"></i></span>
+			<span class="text">${todo.text}</span>
+		</div>
+	`;
+
+	todoList.insertAdjacentHTML('beforeend', html);
+	formInput.value = '';
+	formInput.placeholder = 'What to do?';
 };
 
-// Event listeners
-btnAdd.addEventListener('click', addElement);
+const checkTodo = (key) => {
+	const index = todos.findIndex((item) => item.id === Number(key));
+
+	todos[index].checked = !todos[index].checked;
+	console.log(todos);
+
+	const item = document.querySelector(`[data-key='${key}']`);
+
+	if (todos[index].checked) {
+		item.classList.add('task-done');
+		item.classList.remove('circle');
+		item.classList.add('circle-check');
+
+		// item.forEach((el) => el.classList.add('task-done'));
+	} else {
+		item.classList.remove('task-done');
+	}
+};
